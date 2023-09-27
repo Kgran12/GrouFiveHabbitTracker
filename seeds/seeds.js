400ef8a33cc014bfc87eb542a5fbecb28b69adf7
edward caldwell <caldwell_edward12@yahoo.com>
2023-09-26 20:19:50 -0700

    updated

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -23,3 +23,28 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
+const sequelize = require('../config/connection');
+const { User, Goals } = require('../models');
+
+const userData = require('./userData.json');
+const GoalsData = require('./GoalsData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const Goals of GoalsData) {
+    await Goals.create({
+      ...Goals,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
